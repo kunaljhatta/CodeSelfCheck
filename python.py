587f2865e219f4423ea461ff8e9aae3ca07a1b53
KunalJhatta <Kunaljhatta22@gmail.com>
2022-09-23 20:42:11 -0700

    added a while loop to make the program exit when user types the quit in the promopt

--- python.py
+++ python.py
@@ -1,32 +1,36 @@
 # Name - Kunal Jhatta 
 # Description - This Program asks user to perform calculations, and print results.
 
-prompt = input("SuperMathyboard> ")
+while True:
+    prompt = input("SuperMathyboard> ")
 
-operator = prompt.split(' ', 1)[0]
+    operator = prompt.split(' ', 1)[0]
 
-split_number_string = prompt.split(' ', 1)[1]
+    if operator  == "quit":
+        break
 
-split_third_part = split_number_string.split(' ')[1]
+    split_number_string = prompt.split(' ', 1)[1]
 
-y = split_number_string.split(' ')
-y = [float(i) for i in y]
+    split_third_part = split_number_string.split(' ')[1]
 
-if operator == 'add':
-    w = y[0] + y[1]
-    print(w)
-elif operator == 'sub':
-    w = y[0] - y[1]
-    print(w)
-elif operator == 'mul':
-    w = y[0] * y[1]
-    print(w)
-elif operator == 'div' or split_third_part == 0 and operator == 'div':
-    try:
-        w = y[0] / y[1]
+    y = split_number_string.split(' ')
+    y = [float(i) for i in y]
+
+    if operator == 'add':
+        w = y[0] + y[1]
         print(w)
-    except ZeroDivisionError:
-        print("Can't divide by zero.")
-else:
-    print("usage: add|sub|mul|div v0 v1")
-    print("       quit")
+    elif operator == 'sub':
+        w = y[0] - y[1]
+        print(w)
+    elif operator == 'mul':
+        w = y[0] * y[1]
+        print(w)
+    elif operator == 'div' or split_third_part == 0 and operator == 'div':
+        try:
+            w = y[0] / y[1]
+            print(w)
+        except ZeroDivisionError:
+            print("Can't divide by zero.")
+    else:
+        print("usage: add|sub|mul|div v0 v1")
+        print("       quit")
