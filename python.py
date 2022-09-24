6ddd7be98dda2997151d01545db7b22e8b104f83
KunalJhatta <Kunaljhatta22@gmail.com>
2022-09-23 20:39:46 -0700

    added logic for all the operations and the edge cases also.

--- python.py
+++ python.py
@@ -11,3 +11,22 @@ split_third_part = split_number_string.split(' ')[1]
 
 y = split_number_string.split(' ')
 y = [float(i) for i in y]
+
+if operator == 'add':
+    w = y[0] + y[1]
+    print(w)
+elif operator == 'sub':
+    w = y[0] - y[1]
+    print(w)
+elif operator == 'mul':
+    w = y[0] * y[1]
+    print(w)
+elif operator == 'div' or split_third_part == 0 and operator == 'div':
+    try:
+        w = y[0] / y[1]
+        print(w)
+    except ZeroDivisionError:
+        print("Can't divide by zero.")
+else:
+    print("usage: add|sub|mul|div v0 v1")
+    print("       quit")
