ec41dd293374b93c2f9784e2762f7649325c33db
KunalJhatta <Kunaljhatta22@gmail.com>
2022-09-23 20:22:22 -0700

    converted the string list to numbered list

--- python.py
+++ python.py
@@ -8,3 +8,6 @@ operator = prompt.split(' ', 1)[0]
 split_number_string = prompt.split(' ', 1)[1]
 
 split_third_part = split_number_string.split(' ')[1]
+
+y = split_number_string.split(' ')
+y = [float(i) for i in y]
