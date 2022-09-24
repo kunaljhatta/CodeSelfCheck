5d38a4f6022afdd02a80b8886bc3dfac524274f4
KunalJhatta <Kunaljhatta22@gmail.com>
2022-09-23 21:02:13 -0700

    fixed naming of variables

--- python.py
+++ python.py
@@ -18,22 +18,22 @@ while True:
     split_third_part = split_number_string.split(' ')[1]
     
     # Converting string of lists into numbers list 
-    y = split_number_string.split(' ')
-    y = [float(i) for i in y]
+    list = split_number_string.split(' ')
+    list = [float(i) for i in list]
 
     if operator == 'add':
-        w = y[0] + y[1]
-        print(w)
+        sum = list[0] + list[1]
+        print(sum)
     elif operator == 'sub':
-        w = y[0] - y[1]
-        print(w)
+        sum = list[0] - list[1]
+        print(sum)
     elif operator == 'mul':
-        w = y[0] * y[1]
-        print(w)
+        sum = list[0] * list[1]
+        print(sum)
     elif operator == 'div' or split_third_part == 0 and operator == 'div':
         try:
-            w = y[0] / y[1]
-            print(w)
+            sum = list[0] / list[1]
+            print(sum)
         except ZeroDivisionError:
             print("Can't divide by zero.")
     else:
